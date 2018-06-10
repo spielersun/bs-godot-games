8b0f3bf64eda36193e3a86240daf54c310a57f5b
Güneş Eser <spielersun@gmail.com>
2018-06-10 22:38:32 +0300

    platform jump upd6

---
+++ platform-jump/scripts/grass_platform_break.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+onready var sprite = $Sprite
+onready var collision = $collision
+
+var sprite_half_width
+
+func _ready():
+	sprite_half_width = (sprite.texture.get_size().x) / (2 * scale.x)
+	connect("body_entered", self, "_on_body_entered")
+
+func _on_body_entered(body):
+	if body.name == "player" and !body.jumping:
+		if body.position.y < position.y:
+			body.jump()
+			sprite.queue_free()
+			collision.queue_free()
