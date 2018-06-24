6f041e16f34b3ff1fbb84a2e5190350a06af34f6
Güneş Eser <spielersun@gmail.com>
2018-06-24 20:41:12 +0300

    hammer-time upd6

--- hammer-time/scripts/Nail.gd
+++ hammer-time/scripts/Nail.gd
@@ -1,6 +1,7 @@
 extends Area
 
 onready var animation = $animation
+onready var audio = $audio
 
 var already_hit = false 
 
@@ -10,4 +11,13 @@ func _ready():
 
 func _on_area_entered(area):
 	if area.name == "hammer" and !already_hit:
+		audio.play()
 		animation.play("hit")
+
+
+func _on_VisibilityNotifier_camera_exited(camera):
+	if !already_hit:
+		$"/root/game".load_end_game(score)
+
+func end_game():
+	$"/root/game".load_end_game(score)
