3955acdfa00e4d05ff6c8253487b7563809d9c4e
Güneş Eser <spielersun@gmail.com>
2018-10-18 12:35:42 +0300

    easy b

--- easy-platformer/player.gd
+++ easy-platformer/player.gd
@@ -1,9 +1,9 @@
 extends KinematicBody2D
 
-const UP = Vector2(0,-1)
+const UP = Vector2(0, -1)
 const GRAVITY = 20
 const SPEED = 200
-const JUMP_HEIGHT = -500
+const JUMP_HEIGHT = 500
 
 var motion = Vector2()
 
@@ -12,15 +12,22 @@ func _physics_process(delta):
 	
 	if Input.is_action_pressed("ui_right"):
 		motion.x = SPEED
+		$Sprite.flip_h = false
+		$Sprite.play("run")
 	elif Input.is_action_pressed("ui_left"):
 		motion.x = -SPEED
+		$Sprite.flip_h = true
+		$Sprite.play("run")
 	else: 
+		$Sprite.play("idle")
 		motion.x = 0
 	
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
-			print(motion.y)
+			#print(motion.y)
 			motion.y = -JUMP_HEIGHT
+	else:
+		$Sprite.play("jump")
 		
 	motion = move_and_slide(motion, UP)
 	pass
