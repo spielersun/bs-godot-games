51255547f143068589dc5dfbe24d3ebf736c8ea6
Güneş Eser <spielersun@gmail.com>
2018-10-19 11:21:31 +0300

    peasy b

--- easy-platformer/player.gd
+++ easy-platformer/player.gd
@@ -2,32 +2,44 @@ extends KinematicBody2D
 
 const UP = Vector2(0, -1)
 const GRAVITY = 20
-const SPEED = 200
+const ACCELERATION = 50
+const MAX_SPEED = 200
 const JUMP_HEIGHT = 500
 
 var motion = Vector2()
 
 func _physics_process(delta):
 	motion.y += GRAVITY
+	var friction = false
 	
 	if Input.is_action_pressed("ui_right"):
-		motion.x = SPEED
+		#motion.x += ACCELERATION
+		motion.x = min(motion.x+ACCELERATION, MAX_SPEED)
 		$Sprite.flip_h = false
 		$Sprite.play("run")
 	elif Input.is_action_pressed("ui_left"):
-		motion.x = -SPEED
+		#motion.x -= ACCELERATION
+		motion.x = max(motion.x-ACCELERATION, -MAX_SPEED)
 		$Sprite.flip_h = true
 		$Sprite.play("run")
 	else: 
 		$Sprite.play("idle")
-		motion.x = 0
+		friction = true
 	
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
 			#print(motion.y)
 			motion.y = -JUMP_HEIGHT
+		if friction:
+			motion.x = lerp(motion.x, 0, 0.2)
 	else:
-		$Sprite.play("jump")
+		if motion.y < 0:
+			$Sprite.play("jump")
+		else:
+			$Sprite.play("fall")
+			
+		if friction:
+			motion.x = lerp(motion.x, 0, 0.05)
 		
 	motion = move_and_slide(motion, UP)
 	pass
